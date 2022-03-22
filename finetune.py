a43719d2136da32aa0076b140b27309a955d1795
mxnno <hannes.rupprecht@gmx.de>
2022-03-22 19:58:31 +0100

    fix

--- finetune.py
+++ finetune.py
@@ -152,8 +152,24 @@ def finetune_ADB(args, model, train_dataloader, dev_dataloader):
     return model, centroids, delta
 
 def finetune_imlm(args, model, train_dataloader, dev_dataloader, data_collator, tokenizer ):
-    
-    training_args = TrainingArguments("test-trainer")
+
+    training_args = TrainingArguments(
+        output_dir = args.save_path + "IMLM/Trainer/",
+        per_device_train_batch_size = args.batch_size,
+        learning_rate = args.learning_rate,
+        weight_decay = args.weight_decay,
+        warmup_ratio  = args.warmup_ratio,
+        num_train_epochs = args.num_train_epochs,
+        #später loggen hier (geht mit wandb)
+        logging_strategy = "no",
+        report_to = "wandb",
+        save_strategy  = "no",
+        seed = args.seed
+
+        #load_best_model_at_end = True
+        # -> https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments.load_best_model_at_end
+        #When set to True, the parameters save_strategy needs to be the same as eval_strategy, and in the case it is “steps”, save_steps must be a round multiple of eval_steps.
+        )
     
     trainer = Trainer(
         model,
